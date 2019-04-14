ae2255860802b1ef1f55c80a851a505b5b32a5ce
Jefferson Ramos <jhr.developer@gmail.com>
2019-04-14 19:54:20 -0300

    install node dependencies

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    </head>
+    <body>
+        <div id="app"></div>
+
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
